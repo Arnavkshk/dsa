b25a1ce30c9d5b258480f7c0471a26f5c929e59e
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-08-31 19:52:55 +0530

    practice dsa

---
+++ question_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact=1;
+    for(int i=0;i<n;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+
+int ncr(int n,int r){
+    int ans = 
+}
+
+int main(){
+        
+    return 0;
+}
