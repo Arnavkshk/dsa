0328315f47ee93717b922d7e7f1834900ce05440
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-09-04 23:31:28 +0530

    practice dsa

--- question_6.cpp
+++ question_6.cpp
@@ -3,17 +3,23 @@ using namespace std;
 
 int factorial(int n){
     int fact=1;
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
         fact=fact*i;
     }
     return fact;
 }
 
 int ncr(int n,int r){
-    int ans = 
+    int ans = factorial(n);
+    int denom=factorial(r)*factorial(n-r);
+    int ret= ans/denom;
+    return ret;
 }
 
 int main(){
-        
+        int n,r;
+        cin>>n>>r;
+        cout<<"Answer is "<<ncr(n,r)<<endl;
+
     return 0;
 }
