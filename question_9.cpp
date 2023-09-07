d29560b6adb43c9514fb2ca745f4fd345b923ac2
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-09-07 18:31:53 +0530

    practice dsa

--- question_9.cpp
+++ question_9.cpp
@@ -1,1 +1,26 @@
-i
+#include <iostream>
+using namespace std;
+
+bool isprime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            return 0;
+        }
+       
+       
+    }
+     return 1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    if(isprime(n)){
+        cout<<"is a prime no."<<endl;
+    }
+    else{
+
+        cout<<"not a prime no";
+    }
+    return 0;
+}
