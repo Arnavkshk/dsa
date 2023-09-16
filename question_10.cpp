b67ab04225341be75a83a4cb3cddd2323a983735
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-09-16 12:37:09 +0530

    practice dsa

--- question_10.cpp
+++ question_10.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+int getmax(int num[], int n){
+    int max=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(num[i]>max){
+            max=num[i];
+        }
+    }
+    return max;
+}
+
+int getmin(int num[], int n){
+    int min=INT_MAX;
+    for(int i=0;i<n;i++){
+        if(num[i]<min){
+            min=num[i];
+        }
+    }
+    return min;
+}
+
 int main(){
-    int n;
-    cin >> n;
+    int size;
+    cin >> size;
+    int num[100];
+    for(int i=0;i<size;i++){
+        cin>>num[i];
+    }
+    cout<<"Maximum value is: "<<getmax(num,size)<<endl;
+    cout<<"Minimum value is: "<<getmin(num,size)<<endl;
     return 0;
 }
