4d3c901124d33e8d8b5cef42094ca58fa4a9b8c2
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-09-19 20:39:13 +0530

    practice dsa

---
+++ question_13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[6]={1,4,0,5,-2,15};
+    int brr[5]={2,6,3,9,4};
+
+    reverse(arr,5);
+    reverse(brr,5);
+
+    return 0;
+}
