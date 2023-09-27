6c49520d9402763f53689fd793f8e7cfb4169fcd
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-09-27 22:46:36 +0530

    practice dsa

--- question_17.cpp
+++ question_17.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void sortone(int arr[],int size){
+    int left=0, right = size-1;
+    while(left<right){
+        while (arr[left]==0){
+            left++;
+        }
+        while(arr[right]==1){
+            right--;
+        }
+        if(left<right){
+        swap(arr[left],arr[right]);
+        left++;
+        right--;
+
+        }
+    }
+}
+
+void printarr(int arr[],int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main(){
+    int arr[8]={1,1,0,0,0,0,1,0};
+    sortone(arr,8);
+    printarr(arr,8);
+
     return 0;
 }
