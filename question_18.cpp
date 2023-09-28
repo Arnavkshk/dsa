c1d232e1083bda8366281d5d70c8bb20b067c337
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-09-29 00:24:15 +0530

    practice dsa

--- question_18.cpp
+++ question_18.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+
+int sum_of_even_digits(int n) {
+    int r, sum = 0;
+   
+    while (n > 0) {
+        r = n % 10;    
+        n = n / 10;    
+        if (r % 2 == 0){
+            sum = sum + r;
+        }
+    }
+    return sum;
+}
+int sum_of_odd_digits(int n) {
+    int r, sum = 0;
+   
+    while (n > 0) {
+        r = n % 10;    
+        n = n / 10;    
+        if (r % 2 == 1){
+            sum = sum + r;
+        }
+    }
+    return sum;
+}
+
+int main() {
+     int n;
+    cin>>n;
+    cout<<(sum_of_even_digits(n))<<" "<<(sum_of_odd_digits(n));
+    
     return 0;
 }
