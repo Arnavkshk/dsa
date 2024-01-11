63df444969b884f73267d608099d18af59c8ad3c
arnavkshk <arnav22112003@gmail.com>
2024-01-11 11:54:45 +0530

    practice das

--- question_21.cpp
+++ question_21.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
+    int n=1234;
+    int ans=0;
+    while(n!=0){
+        int digit=n%10;
+        ans=(ans*10)+digit;
+        n=n/10;
+    }
+        cout<<ans;
     return 0;
 }
