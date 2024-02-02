5c61b706268197c8306a541d9145cb1ed77ea174
arnavkshk <arnav22112003@gmail.com>
2024-02-02 23:20:34 +0530

    practice dsa

--- practice_13.cpp
+++ practice_13.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+long long int binarySearch(int arr[],int size,int key){
+
+    int start=0;
+    int end=size-1;
+    int mid = (start+end)/2;
+
+    while(start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        if(key>arr[mid]){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+        mid=(start+end)/2;
+    }
+    return -1;
+
+}
+
 int main(){
     
     return 0;
