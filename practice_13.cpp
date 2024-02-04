fd97fcce5404a8c4c658eba7e30753f23926ea45
arnavkshk <arnav22112003@gmail.com>
2024-02-04 22:22:26 +0530

    practice dsa

--- practice_13.cpp
+++ practice_13.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-long long int sqrtInteger(int arr[],int n,int key){
+long long int sqrtInteger(int n){
 
     int s=0;
     int e=n;
     long long int mid =  s+(e-s)/2;
     long long int ans=-1;
     while(s<=e){
-        if(arr[mid]==key){
+        long long int square=mid*mid;
+        if(square==n){
             return mid;
         }
-        if(key>arr[mid]){
+        if(square<n){
+            ans = mid;
             s=mid+1;
         }
         else{
@@ -23,7 +25,22 @@ long long int sqrtInteger(int arr[],int n,int key){
 
 }
 
+double moreprecision(int n,int precision,int tempsol){
+    double factor = 1;
+    double ans=tempsol;
+    for(int i=0;i<precision;i++){
+        factor=factor/10;
+        for(double j=ans;j*j<n;j=j+factor){
+            ans=j;
+        }
+    } 
+    return ans;
+}
 int main(){
-    
+    int n;
+    cout<<"enter the no"<<endl;
+    cin>>n;
+    int tempsol=sqrtInteger(n);   
+    cout<<"answer is "<<moreprecision(n,3,tempsol);
     return 0;
 }
