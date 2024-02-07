fc635cf1d075cdf663f1f788fbbb8399db7d9186
arnavkshk <arnav22112003@gmail.com>
2024-02-07 22:54:55 +0530

    practice dsa

--- practice_14.cpp
+++ practice_14.cpp
@@ -8,5 +8,18 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+    for(int i=0;i<n-1;i++){
+        int minindex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minindex]){
+                minindex=j;
+            }
+        }
+        swap(arr[minindex],arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
     return 0;
 }
