177ab85baaf90d0ae0849a5bd4ab7a2214f367aa
arnavkshk <arnav22112003@gmail.com>
2024-02-10 19:49:37 +0530

    practice dsa

--- practice_15.cpp
+++ practice_15.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    int arr[100];
-    int n;
+    int n,temp;
     cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+              temp=arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=arr[j];  
+            }
+        }
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+    }
     return 0;
 }
