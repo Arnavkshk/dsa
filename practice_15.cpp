bfee45e287cb4542b881fb13f9c9ee02ed89dd75
arnavkshk <arnav22112003@gmail.com>
2024-02-10 19:51:35 +0530

    practice dsa

--- practice_15.cpp
+++ practice_15.cpp
@@ -8,17 +8,17 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=1;i<n;i++){
-        for(int j=0;j<n-i;j++){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
               temp=arr[j];
               arr[j]=arr[j+1];
-              arr[j+1]=arr[j];  
+              arr[j+1]=temp;  
             }
         }
+    }
         for(int i=0;i<n;i++){
             cout<<arr[i]<<" ";
         }
-    }
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,3 @@
-
-    // int i=1;
-    // while (i<=n){ 
-    //     int j=1;
-    //     while(j<=n-i+1){
-    //         cout<<j;
-    //         j++;            
-    //     }
-    //     int space = j;
-    //     while(space<=n){
-    //         cout<<"*";
-    //         space=space+1;
-    //     }
-    //     int space1=1;
-    //     while(space1<=i-1){
-    //         cout<<"*";
-    //         space1=space1+1;
-    //     }
+for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
