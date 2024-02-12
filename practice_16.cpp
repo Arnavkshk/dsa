fb9aed3fb56e432f7f2df2359cd67804a6ff1a1c
arnavkshk <arnav22112003@gmail.com>
2024-02-12 18:46:02 +0530

    practice dsa

--- practice_16.cpp
+++ practice_16.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
+
 int main(){
+    list<int> l;
+    l.push_back(2);
+    l.push_front(1);
+    for(int i:l){
+        cout<<i<<" ";
+    }
     return 0;
 }
