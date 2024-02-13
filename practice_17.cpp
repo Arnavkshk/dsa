5fb39b3382d5f0e65f80bf2a91ad17176d98dfb9
arnavkshk <arnav22112003@gmail.com>
2024-02-13 20:06:20 +0530

    practice dsa

--- practice_17.cpp
+++ practice_17.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int n=sizeof(arr)/sizeof(int);
+    for(int i=0;i<5;i++){
+        arr[i]=arr[n-1];
+    }
     return 0;
 }
