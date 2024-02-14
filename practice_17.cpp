a48e6f4c753898feb91596c213377fb705d02b27
arnavkshk <arnav22112003@gmail.com>
2024-02-14 18:40:46 +0530

    practice dsa

--- practice_17.cpp
+++ practice_17.cpp
@@ -4,8 +4,13 @@ using namespace std;
 int main(){
     int arr[]={1,2,3,4,5,6,7,8,9,10};
     int n=sizeof(arr)/sizeof(int);
-    for(int i=0;i<5;i++){
-        arr[i]=arr[n-1];
+    for(int i=0;i<n/2;i++){
+        int temp = arr[i];
+        arr[i]=arr[n-1-i];
+        arr[n-1-i]=temp;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
     return 0;
 }
