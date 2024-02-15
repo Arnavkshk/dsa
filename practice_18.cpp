d6ff7d05d65d4d104d3a3d265a42daacb68eca59
arnavkshk <arnav22112003@gmail.com>
2024-02-15 19:11:05 +0530

    practice dsa

--- practice_18.cpp
+++ practice_18.cpp
@@ -3,11 +3,26 @@ using namespace std;
 
 //program for merge sorted array
 
-int mergesorted(int arr1[],int n,int arr2[],int m,int arr3[]){
-
-
-
+void mergesorted(int arr1[],int n,int arr2[],int m,int arr3[]){
+    int i=0,j=0,k=0;
+    while(i<n && j<m){
+        if(arr1[i]<arr2[j]){
+            arr3[k++]=arr1[i++];
+        }
+            else{
+                arr3[k++]=arr2[j++];
+            }
+        }
+    while(i<n){
+        arr3[k++]=arr1[i++];
+    }
+    while(j<m){
+        arr3[k++]=arr2[j++];
+    }
 }
+
+
+
 
 void printarr(int arr3[],int n){
     for(int i=0;i<n;i++){
@@ -16,13 +31,13 @@ void printarr(int arr3[],int n){
 }
 
 int main(){
-    int arr1[5]={1,3,5,7,9};
+    int arr1[8]={1,3,5,7,9,11,13,15};
     int arr2[4]={2,4,6,8};
-    int arr3[9]={0};
+    int arr3[12]={0};
 
-    mergesorted(arr1,5,arr2,4,arr3);
+    mergesorted(arr1,8,arr2,4,arr3);
 
-    printarr(arr3,9);
+    printarr(arr3,12);
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,10 @@
 for(int i=0;i<n;i++){
-            cout<<arr[i]<<" ";
-        }
+    //     for(int j=0;j<m;j++){
+    //         if(arr1[i]<arr2[j]){
+    //             arr3[i]=arr1[i];
+    //         }
+    //         else{
+    //             arr3[i]=arr2[i];
+    //         }
+    //     }
+    // }
