e03dd0daa78d3003f3aa5f0126c1908ce47921a7
arnavkshk <arnav22112003@gmail.com>
2024-02-17 17:23:31 +0530

    practice dsa

--- practice_19.cpp
+++ practice_19.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
+    int n =3;
+    vector<int> arr;
+    arr.push_back(2);
+    arr.push_back(3);
+    arr.push_back(4);
+    arr.push_back(5);
+    arr.push_back(6);
+
+    vector<int> temp(arr.size());
+    for(int i=0;i<arr.size();i++){
+        temp[(i+n)%arr.size()]=arr[i];
+    }
+    arr=temp;
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<< " ";
+    }
+
     return 0;
 }
