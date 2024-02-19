10e0179a2f542f3bb19345821c8bbfb75cd9ec77
arnavkshk <arnav22112003@gmail.com>
2024-02-19 18:17:41 +0530

    practice dsa

--- practice_20.cpp
+++ practice_20.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int getLength(char name[]){
+    int count = 0;
+    for(int i=0;name[i]!='\0';i++){
+        count++;
+    }
+    return count;
+}
+
 int main(){
-auto lmao='c';
-cout<<lmao;
+    char name[20];
+    cout<<"enter the string "<<endl;
+    cin>>name;
+    cout<<"the string is "<<name<<endl;
+    cout<<"length of string = "<<getLength(name);
 
     return 0;
 }
