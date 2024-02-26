9bfa180726cdde1e5807fecb529218af6c38fd07
arnavkshk <arnav22112003@gmail.com>
2024-02-26 13:29:40 +0530

    practice dsa

--- practice_24.cpp
+++ practice_24.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+//pointers
+
 int main(){
-    
+    int num = 5;
+    cout<<num<<endl;
+    cout<<"address of num is "<<&num<<endl;
+    int *ptr = &num;
+    cout<<++(*ptr);
     return 0;
 }
