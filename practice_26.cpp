96af0e282591bfedb7a130abf40574d8f7e39d66
arnavkshk <arnav22112003@gmail.com>
2024-02-29 19:32:19 +0530

    practice dsa

---
+++ practice_26.cpp
@@ -0,0 +1,15 @@
+//reference variable 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=5;
+    int &j=i;
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+    j++;
+    cout<<i<<endl;
+
+    return 0;
+}
