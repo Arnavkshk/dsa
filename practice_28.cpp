54d9d1c5cffa9004da7125ef899b07ec24c72b7a
arnavkshk <arnav22112003@gmail.com>
2024-03-01 19:22:26 +0530

    practice dsa

---
+++ practice_28.cpp
@@ -0,0 +1,29 @@
+//creating dynamic 2-D array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int **arr=new int*[n];
+
+    for(int i=0;i<n;i++){
+        arr[i]=new int[n];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
