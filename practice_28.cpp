e6742b3601ab7472d24fed31be52a1b814577925
arnavkshk <arnav22112003@gmail.com>
2024-03-02 12:43:19 +0530

    practice dsa

--- practice_28.cpp
+++ practice_28.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
-    int **arr=new int*[n];
+    int row, col;
+    cin>>row>>col;
+    int **arr=new int*[row];
 
-    for(int i=0;i<n;i++){
-        arr[i]=new int[n];
+    for(int i=0;i<row;i++){
+        arr[i]=new int[col];
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
             cin>>arr[i][j];
         }
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
