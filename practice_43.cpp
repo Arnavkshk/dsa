e71ebd58e1699d63f1ecd19584636d0d2bec8547
arnavkshk <arnav22112003@gmail.com>
2024-03-10 19:52:31 +0530

    practice dsa

--- practice_43.cpp
+++ practice_43.cpp
@@ -3,18 +3,28 @@
 using namespace std;
 
 class Hero{
+private:
 public:
     int health;
     char level;
-    
+
+    int gethealth(){
+        return health; 
+    }
+    void sethealth(int h){
+        health = h;
+    }
 
 };
 
 int main(){
     Hero ramesh;
-    ramesh.health =70;
+    Hero *b = new Hero;
+    b->sethealth(30);
+    cout<<"health of b is "<<b->health<<endl;
+    ramesh.sethealth(20);
     ramesh.level = 'A';
-    cout<<"health is "<<ramesh.health<<endl;
+    cout<<"health is "<<ramesh.gethealth()<<endl;
     cout<<"level is "<<ramesh.level<<endl;
 
     return 0;
